9071c28b184ae996e492a507085d7faacf996130
LinB203 <2267330597@qq.com>
2023-11-21 10:35:46 +0800

    fix video

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -22,7 +22,7 @@ def build_image_tower(image_tower_cfg, **kwargs):
 
 def build_video_tower(video_tower_cfg, **kwargs):
     video_tower = getattr(video_tower_cfg, 'mm_video_tower', getattr(video_tower_cfg, 'video_tower', None))
-    if video_tower.endswith('LanguageBind_Video'):
+    if video_tower.endswith('LanguageBind_Video_merge'):
         return LanguageBindVideoTower(video_tower, args=video_tower_cfg, cache_dir='./cache_dir', **kwargs)
     raise ValueError(f'Unknown video tower: {video_tower}')
 
