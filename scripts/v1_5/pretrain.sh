aeefb1ad4e2a4e4f9efa5028f85ff82c08b0f602
lb203 <62638829+LinB203@users.noreply.github.com>
2024-01-16 11:41:08 +0800

    Update pretrain.sh

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -1,26 +1,28 @@
 
-DATA_ROOT="llava_all_image_video"
-CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed llava/train/train_mem.py \
+JSON_FOLDER="llava_all_image_video/pt_json"
+IMAGE_FOLDER="llava_all_image_video"
+VIDEO_FOLDER="llava_all_image_video"
+cd /path/to/Video-LLaVA
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 deepspeed videollava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
-    --version plain \
-    --data_path train_json/valley_llavaimage.json \
-    --video_folder ${DATA_ROOT} \
-    --image_folder ${DATA_ROOT} \
-    --X "Video" "Image" \
-    --video_tower LanguageBind/LanguageBind_Video_merge \
+    --version v1 \
+    --data_path ${JSON_FOLDER}/llava_image_.json ${JSON_FOLDER}/valley_.json \
+    --image_folder ${IMAGE_FOLDER} \
     --image_tower LanguageBind/LanguageBind_Image \
+    --video_folder ${VIDEO_FOLDER} \
+    --video_tower LanguageBind/LanguageBind_Video_merge \
     --mm_projector_type mlp2x_gelu \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
-    --mm_use_x_start_end False \
-    --mm_use_x_patch_token False \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ./checkpoints/Video-LLaVA-Pretrain-7B \
+    --output_dir ./checkpoints/videollava-7b-pretrain \
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
