e6a2504320fc9343079d846724948124ffe1e18a
lb203 <62638829+LinB203@users.noreply.github.com>
2024-01-16 11:42:25 +0800

    Update finetune.sh

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -1,29 +1,32 @@
 
 
-DATA_ROOT="llava_all_image_video"
-HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed llava/train/train_mem.py \
-    --deepspeed ./scripts/zero2.json \
+JSON_FOLDER="llava_all_image_video/ft_json"
+IMAGE_FOLDER="llava_all_image_video"
+VIDEO_FOLDER="llava_all_image_video"
+cd /path/to/Video-LLaVA
+
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 deepspeed videollava/train/train_mem.py \
+    --deepspeed ./scripts/zero2_offload.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path train_json/videochatgpt_llavaimage_tune.json \
-    --video_folder ${DATA_ROOT} \
-    --image_folder ${DATA_ROOT} \
-    --X "Video" "Image" \
-    --video_tower LanguageBind/LanguageBind_Video_merge \
+    --data_path ${JSON_FOLDER}/llava_image_tune_.json ${JSON_FOLDER}/videochatgpt_tune_.json ${JSON_FOLDER}/nlp_tune.json \
+    --image_folder ${IMAGE_FOLDER} \
     --image_tower LanguageBind/LanguageBind_Image \
-    --pretrain_mm_mlp_adapter checkpoints/Video-LLaVA-Pretrain-7B/mm_projector.bin \
+    --video_folder ${VIDEO_FOLDER} \
+    --video_tower LanguageBind/LanguageBind_Video_merge \
     --mm_projector_type mlp2x_gelu \
+    --pretrain_mm_mlp_adapter ./checkpoints/videollava-7b-pretrain/mm_projector.bin \
     --mm_vision_select_layer -2 \
-    --mm_use_x_start_end False \
-    --mm_use_x_patch_token False \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/Video-LLaVA-7B \
+    --output_dir ./checkpoints/videollava-7b \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
@@ -36,7 +39,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 CUDA_VISIBLE_DEVICES=0,1,2,3 deepsp
     --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 8 \
+    --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to tensorboard \
-    --cache_dir "./cache_dir"
+	--cache_dir "./cache_dir"
