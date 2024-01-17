c269d1f331bd01122993de99fc10264ce57b6bdc
lb203 <62638829+LinB203@users.noreply.github.com>
2024-01-17 16:48:49 +0800

    Update finetune.sh

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -42,4 +42,4 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 deepspeed videollava/train/train_me
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to tensorboard \
-	--cache_dir "./cache_dir"
+    --cache_dir "./cache_dir"
