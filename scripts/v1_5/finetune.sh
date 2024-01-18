b272b469aed41955ee69f1fc21deab94711ce15c
lb203 <62638829+LinB203@users.noreply.github.com>
2024-01-18 10:47:31 +0800

    Update finetune.sh

--- scripts/v1_5/finetune.sh
+++ scripts/v1_5/finetune.sh
@@ -37,7 +37,7 @@ HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 deepspeed videollava/train/train_me
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 True \
-    --model_max_length 2048 \
+    --model_max_length 2048  --tokenizer_model_max_length 3072 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
