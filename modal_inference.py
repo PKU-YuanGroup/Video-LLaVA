660189cfb59018ad8b41c3cdeb3b9054165e1b28
Ben Schreck <ben@benschreck.com>
2024-02-12 15:34:08 -0800

    modal working

--- modal_inference.py
+++ modal_inference.py
@@ -1,211 +1,144 @@
-from modal import (
-    Cls,
-    method,
-    enter,
-    web_endpoint,
-)
+import os
+import urllib
+
+from modal import asgi_app, method, enter, build
+from .stub import VOLUME_DIR, MODEL_CACHE, cls_dec, function_dec, volume
 from pathlib import Path
-from .stub import stub, cls_dec, function_dec, MODELS_DIR, volume, HF_DATASETS_CACHE, REPO_HOME, load_pretrained_from_cache
+# for local testing
+#VOLUME_DIR = "volume"
+#MODEL_CACHE = "models"
+#Path(VOLUME_DIR).mkdir(exist_ok=True, parents=True)
+VIDEOS_DIR = Path(VOLUME_DIR) / "videos"
+IMAGES_DIR = Path(VOLUME_DIR) / "images"
 
 
-LOCAL_VIDEOS_PATH = Path(REPO_HOME) / "downloaded_videos"
-DEFAULT_PROMPT = "describe what is going on in this video"
-
-
-#  def load_pretrained_from_cache(load_4bit=True, load_8bit=False):
-    #  print("Loading pretrained model")
-    #  from videollava.utils import disable_torch_init
-    #  from transformers import AutoTokenizer, BitsAndBytesConfig
-    #  from videollava.model import LlavaLlamaForCausalLM
-    #  import torch
-    #  disable_torch_init()
-    #  print("imported")
-
-    #  kwargs = {
-        #  "device_map": "auto",
-        #  "cache_dir": HF_DATASETS_CACHE,
-    #  }
-    #  video_llava_path = Path(MODELS_DIR) / 'Video-LLaVA-7B'
-
-    #  vlp_exists = video_llava_path.exists()
-    #  if not vlp_exists:
-        #  video_llava_path.mkdir(exist_ok=True, parents=True)
-
-    #  save = False
-    #  if not video_llava_path.exists() or len(list(video_llava_path.iterdir())) == 0:
-        #  save = True
-        #  print("Downloading model")
-        #  video_llava_path = 'LanguageBind/Video-LLaVA-7B'
-
-    #  tokenizer_path = Path(MODELS_DIR) / 'tokenizer'
-    #  if not tokenizer_path.exists() or len(list(tokenizer_path.iterdir())) == 0:
-        #  print("Downloading tokenizer")
-        #  tokenizer_path = 'LanguageBind/Video-LLaVA-7B'
-
-    #  if load_8bit:
-        #  kwargs['load_in_8bit'] = True
-    #  elif load_4bit:
-        #  kwargs['load_in_4bit'] = True
-        #  kwargs['quantization_config'] = BitsAndBytesConfig(
-            #  load_in_4bit=True,
-            #  bnb_4bit_compute_dtype=torch.float16,
-            #  bnb_4bit_use_double_quant=True,
-            #  bnb_4bit_quant_type='nf4'
-        #  )
-    #  else:
-        #  kwargs['torch_dtype'] = torch.float16
-
-    #  tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=False, cache_dir=kwargs["cache_dir"])
-    #  model = LlavaLlamaForCausalLM.from_pretrained(video_llava_path, low_cpu_mem_usage=True, **kwargs)
-    #  model.generation_config.do_sample = True
-
-    #  if save:
-        #  # save to on-disk paths
-        #  video_llava_path = Path(MODELS_DIR) / 'Video-LLaVA-7B'
-        #  tokenizer_path = Path(MODELS_DIR) / 'tokenizer'
-        #  tokenizer.save_pretrained(str(tokenizer_path))
-        #  model.save_pretrained(str(video_llava_path))
-    #  return model, tokenizer
-
-def prepare_processor(model, device="cuda"):
-    import torch
-    processor = {'image': None, 'video': None}
-    if model.config.mm_image_tower is not None:
-        image_tower = model.get_image_tower()
-        if not image_tower.is_loaded:
-            image_tower.load_model()
-        image_tower.to(device=device, dtype=torch.float16)
-        image_processor = image_tower.image_processor
-        processor['image'] = image_processor
-    if model.config.mm_video_tower is not None:
-        video_tower = model.get_video_tower()
-        if not video_tower.is_loaded:
-            video_tower.load_model()
-        video_tower.to(device=device, dtype=torch.float16)
-        video_processor = video_tower.video_processor
-        processor['video'] = video_processor
-    return processor
-
-def prepare_special_tokens(model, tokenizer):
-    from videollava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, \
-        DEFAULT_VIDEO_PATCH_TOKEN, DEFAULT_VID_START_TOKEN, DEFAULT_VID_END_TOKEN
-    mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
-    mm_use_im_patch_token = getattr(model.config, "mm_use_im_patch_token", True)
-    if mm_use_im_patch_token:
-        tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
-        tokenizer.add_tokens([DEFAULT_VIDEO_PATCH_TOKEN], special_tokens=True)
-    if mm_use_im_start_end:
-        tokenizer.add_tokens([DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True)
-        tokenizer.add_tokens([DEFAULT_VID_START_TOKEN, DEFAULT_VID_END_TOKEN], special_tokens=True)
-    model.resize_token_embeddings(len(tokenizer))
-
-@cls_dec(container_idle_timeout=30, gpu='L4')
-class VideoLlava:
-    def __init__(self, device='cuda'):
-        self.device = device
-        self.model = None
-    # TODO when they fix
-    #@build()
+@cls_dec(gpu="any")
+class VideoLlavaModel:
+    @build()
     @enter()
     def load_model(self):
-        self.model, self.tokenizer = load_pretrained_from_cache(load_4bit=False, load_8bit=True)
-        print("got model")
-        self.processor = prepare_processor(self.model, device=self.device)
-        self.video_processor = self.processor['video']
-
-    def prepare_conv(self):
-        from videollava.conversation import conv_templates
-        self.conv = conv_templates["llava_v1"].copy()
-        self.roles = self.conv.roles
+        import torch
+        from videollava.serve.gradio_utils import Chat
+        self.conv_mode = "llava_v1"
+        model_path = 'LanguageBind/Video-LLaVA-7B'
+        device = 'cuda'
+        load_8bit = False
+        load_4bit = True
+        self.dtype = torch.float16
+        self.handler = Chat(model_path, conv_mode=self.conv_mode, load_8bit=load_8bit, load_4bit=load_4bit, device=device, cache_dir=str(MODEL_CACHE))
+        print("model loaded")
+        # self.handler.model.to(dtype=self.dtype)
 
     @method()
-    def inference(self, video_path: str, inp: str = DEFAULT_PROMPT):
-        import torch
-        from videollava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN
-        from videollava.mm_utils import tokenizer_image_token, KeywordsStoppingCriteria
-        from videollava.conversation import SeparatorStyle
-        import requests
-        from io import BytesIO
-        print('preparing conv')
+    def generate(self, image1, video, textbox_in):
+        from videollava.conversation import conv_templates
+        from videollava.constants import DEFAULT_IMAGE_TOKEN
+        if not textbox_in:
+            raise ValueError("no prompt provided")
 
-        self.prepare_conv()
-        if video_path.startswith("http"):
-            print("Downloading video")
-            video_bytes = requests.get(video_path).content
-            local_video_path = LOCAL_VIDEOS_PATH / video_path.split("/")[-1]
-            if not LOCAL_VIDEOS_PATH.exists():
-                LOCAL_VIDEOS_PATH.mkdir(exist_ok=True, parents=True)
-            with open(local_video_path, "wb") as f:
-                f.write(video_bytes)
-            video_path = BytesIO(video_bytes)
-            print(f"Downloaded video and saved to {local_video_path}")
-        elif not Path(video_path).exists():
-            volume.reload()
-            if not Path(video_path).exists():
-                raise FileNotFoundError(f"Video {video_path} not found")
-        print('processing video')
-        video_tensor = self.video_processor(video_path, return_tensors='pt')['pixel_values']
-        if type(video_tensor) is list:
-            tensor = [video.to(self.model.device, dtype=torch.float16) for video in video_tensor]
+        image1 = image1 if image1 else "none"
+        video = video if video else "none"
+
+        state_ = conv_templates[self.conv_mode].copy()
+        images_tensor = []
+
+        text_en_in = textbox_in.replace("picture", "image")
+
+        image_processor = self.handler.image_processor
+        if os.path.exists(image1) and not os.path.exists(video):
+            tensor = image_processor.preprocess(image1, return_tensors='pt')['pixel_values'][0]
+            tensor = tensor.to(self.handler.model.device, dtype=self.dtype)
+            images_tensor.append(tensor)
+        video_processor = self.handler.video_processor
+        if not os.path.exists(image1) and os.path.exists(video):
+            tensor = video_processor(video, return_tensors='pt')['pixel_values'][0]
+            tensor = tensor.to(self.handler.model.device, dtype=self.dtype)
+            images_tensor.append(tensor)
+        if os.path.exists(image1) and os.path.exists(video):
+            tensor = video_processor(video, return_tensors='pt')['pixel_values'][0]
+            tensor = tensor.to(self.handler.model.device, dtype=self.dtype)
+            images_tensor.append(tensor)
+
+            tensor = image_processor.preprocess(image1, return_tensors='pt')['pixel_values'][0]
+            tensor = tensor.to(self.handler.model.device, dtype=self.dtype)
+            images_tensor.append(tensor)
+
+        if os.path.exists(image1) and not os.path.exists(video):
+            text_en_in = DEFAULT_IMAGE_TOKEN + '\n' + text_en_in
+        elif not os.path.exists(image1) and os.path.exists(video):
+            text_en_in = ''.join([DEFAULT_IMAGE_TOKEN] * self.handler.model.get_video_tower().config.num_frames) + '\n' + text_en_in
+        elif os.path.exists(image1) and os.path.exists(video):
+            text_en_in = ''.join([DEFAULT_IMAGE_TOKEN] * self.handler.model.get_video_tower().config.num_frames) + '\n' + text_en_in + '\n' + DEFAULT_IMAGE_TOKEN
         else:
-            tensor = video_tensor.to(self.model.device, dtype=torch.float16)
+            print("WARNING: No image or video supplied")
 
-        inp = ' '.join([DEFAULT_IMAGE_TOKEN] * self.model.get_video_tower().config.num_frames) + '\n' + inp
-        self.conv.append_message(self.conv.roles[0], inp)
-        self.conv.append_message(self.conv.roles[1], None)
-        prompt = self.conv.get_prompt()
-        input_ids = tokenizer_image_token(prompt, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).cuda()
-        stop_str = self.conv.sep if self.conv.sep_style != SeparatorStyle.TWO else self.conv.sep2
-        keywords = [stop_str]
-        stopping_criteria = KeywordsStoppingCriteria(keywords, self.tokenizer, input_ids)
-        print(input_ids.shape, tensor.shape)
+        print(text_en_in)
+        text_en_out, _ = self.handler.generate(images_tensor, text_en_in, first_run=True, state=state_)
 
-        import torch
-        import time
-        begin = time.time()
-        with torch.inference_mode():
-            output_ids = self.model.generate(
-                input_ids,
-                images=tensor,
-                do_sample=True,
-                temperature=0.9,
-                max_new_tokens=1024,
-                use_cache=True,
-                stopping_criteria=[stopping_criteria])
-        end = time.time()
-        print(f"Generate time taken: {end-begin}")
+        text_en_out = text_en_out.split('#')[0]
+        textbox_out = text_en_out
 
-        output = self.tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
-        print(output)
-        return output
+        return textbox_out
 
-@function_dec(gpu='L4')
-@web_endpoint()
-def inference(video_path: str, inp: str):
-    video_llava = VideoLlava()
-    if not hasattr(video_llava, 'model') or video_llava.model is None:
-        print('loading model')
-        video_llava.load_model()
-    print('model loaded')
-    output = VideoLlava().inference(video_path, inp)
-    print(output)
-    return output
 
-@stub.local_entrypoint()
-def main():
-    prompt = "describe what is going on in this video"
-    video_path = '/volume/pika_water_city.mp4'
-    input_ids, tensor, stopping_criteria = VideoLlava().get_inputs.remote(video_path, prompt)
-    output = VideoLlava().inference.remote(input_ids, tensor, stopping_criteria)
-    print(output)
 
-if __name__ == "__main__":
-    video_llava = Cls.lookup("updated-video-llava-ephemeral", 'VideoLlava')()
-    video_path = '/volume/pika_water_city.mp4'
-    #print(video_llava.inference.remote(video_path=video_path, inp="describe what is going on in this video"))
+def fastapi_app():
+    from fastapi import FastAPI, UploadFile, File
+    import aiofiles
 
-    prompt = "describe what is going on in this video"
-    video_path = '/volume/pika_water_city.mp4'
-    output = video_llava.inference.remote(video_path, prompt)
-    print(output)
+    Path(MODEL_CACHE).mkdir(exist_ok=True, parents=True)
+    VIDEOS_DIR.mkdir(exist_ok=True, parents=True)
+    IMAGES_DIR.mkdir(exist_ok=True, parents=True)
 
+    app = FastAPI()
+    model = VideoLlavaModel()
+
+    @app.post("/upload")
+    async def upload(
+        file: UploadFile = File(...),
+    ):
+        filename_decoded = urllib.parse.unquote(file.filename)
+        file_path = str(VIDEOS_DIR / filename_decoded)
+        async with aiofiles.open(file_path, "wb") as buffer:
+            while content := await file.read(1024):  # Read chunks of 1024 bytes
+                await buffer.write(content)
+        volume.commit()
+        return {"file_path": file_path}
+
+    @app.post("/inference")
+    async def inference(
+        video_file_name: str = '',
+        video_file_path: str = '',
+        image_file_name: str = '',
+        image_file_path: str = '',
+        prompt: str = '',
+    ):
+        import requests
+        requests.get('https://huggingface.co/LanguageBind/Video-LLaVA-7B/resolve/main/config.json').raise_for_status()
+        video_file_name = urllib.parse.unquote(video_file_name)
+        video_file_path = urllib.parse.unquote(video_file_path)
+        if video_file_path is None or video_file_path == '':
+            if video_file_name is None or video_file_name == '':
+                raise ValueError("one of video_file_path or video_file_name must be specified")
+            video_file_path = str(VIDEOS_DIR / video_file_name)
+
+        image_file_name = urllib.parse.unquote(image_file_name)
+        image_file_path = urllib.parse.unquote(image_file_path)
+        if image_file_path is None or image_file_path == '':
+            if image_file_name is not None and image_file_name != '':
+                image_file_path = str(IMAGES_DIR / image_file_name)
+
+        return model.generate.remote(image_file_path, video_file_path, prompt)
+    return app
+
+
+@function_dec()
+@asgi_app()
+def fastapi_app_modal():
+    return fastapi_app()
+
+# local testing:
+# comment this out to deploy
+# app = fastapi_app()
+# conda activate videollava
+# uvicorn modal_inference:app
--- stub.py
+++ stub.py
@@ -1,4 +1,4 @@
-from modal import Volume, Image, Stub, Mount, Secret, build
+from modal import Volume, Image, Stub, Mount, Secret
 from pathlib import Path
 REPO_HOME = "/app"
 VOLUME_DIR = "/volume"
@@ -18,11 +18,14 @@ volumes = {VOLUME_DIR: volume}
 stub = Stub("updated-video-llava", mounts=mounts, volumes=volumes, secrets=[Secret.from_dotenv()])
 
 
+def remove_old_files():
+    import shutil
+    shutil.rmtree('/volume/models', ignore_errors=True)
+
 image = (
     Image.from_registry(
         "nvidia/cuda:12.1.1-cudnn8-devel-ubuntu22.04", add_python="3.11"
     )
-    #Image.debian_slim()
     .apt_install(
         "git",
         "curl",
@@ -56,36 +59,6 @@ image = (
         "python -m bitsandbytes",
         gpu="any"
     )
-    #  .pip_install(
-        #  "torchvision>=0.15.2",
-        #  #"tokenizers>=0.12.1,<0.14",
-        #  "sentencepiece==0.1.99",
-        #  "shortuuid",
-        #  "accelerate==0.21.0",
-        #  "peft==0.4.0",
-        #  "pydantic<2,>=1",
-        #  "markdown2[all]",
-        #  "numpy",
-        #  "scikit-learn==1.2.2",
-        #  "gradio==3.37.0",
-        #  "gradio_client==0.7.0",
-        #  "requests",
-        #  "httpx==0.24.0",
-        #  "uvicorn",
-        #  "fastapi",
-        #  #"einops==0.6.1",
-        #  "einops-exts==0.0.4",
-        #  "timm==0.6.13",
-        #  "deepspeed==0.9.5",
-        #  "ninja",
-        #  "wandb",
-        #  "tensorboardX==2.6.2.2",
-
-        #  "tenacity",
-        #  "torch==2.1.2",
-        #  "wheel",
-        #  gpu="any",
-    #  )
     .run_commands("pip install flash-attn --no-build-isolation", gpu="any")
     .env({"PYTHONPATH": REPO_HOME, "HF_DATASETS_CACHE": HF_DATASETS_CACHE})
     .pip_install(
@@ -94,14 +67,19 @@ image = (
         "git+https://github.com/facebookresearch/pytorchvideo.git@28fe037d212663c6a24f373b94cc5d478c8c1a1d",
         gpu="any",
     )
-    #.run_function(load_pretrained_from_cache, gpu="any")
+    .pip_install(
+        "aiofiles",
+    )
+    .run_function(remove_old_files)
 )
+# TODO bitsandbytes seems to not be working with gpu
 
 def function_dec(**extras):
     return stub.function(
         image=image,
         timeout=80000,
-        checkpointing_enabled=True,  # Enable memory checkpointing for faster cold starts.
+        # checkpointing doesn't work because it restricts internet access
+        #checkpointing_enabled=True,  # Enable memory checkpointing for faster cold starts.
         _allow_background_volume_commits=True,
         **extras,
     )
@@ -110,61 +88,7 @@ def cls_dec(**extras):
     return stub.cls(
         image=image,
         timeout=80000,
-        checkpointing_enabled=True,  # Enable memory checkpointing for faster cold starts.
+        # checkpointing doesn't work because it restricts internet access
+        #checkpointing_enabled=True,  # Enable memory checkpointing for faster cold starts.
         **extras,
     )
-
-def load_pretrained_from_cache(load_4bit=True, load_8bit=False):
-    print("Loading pretrained model")
-    from videollava.utils import disable_torch_init
-    from transformers import AutoTokenizer, BitsAndBytesConfig
-    from videollava.model import LlavaLlamaForCausalLM
-    import torch
-    disable_torch_init()
-    print("imported")
-
-    kwargs = {
-        "device_map": "auto",
-        "cache_dir": HF_DATASETS_CACHE,
-    }
-    video_llava_path = Path(MODELS_DIR) / 'Video-LLaVA-7B'
-
-    vlp_exists = video_llava_path.exists()
-    if not vlp_exists:
-        video_llava_path.mkdir(exist_ok=True, parents=True)
-
-    save = False
-    if not video_llava_path.exists() or len(list(video_llava_path.iterdir())) == 0:
-        save = True
-        print("Downloading model")
-        video_llava_path = 'LanguageBind/Video-LLaVA-7B'
-
-    tokenizer_path = Path(MODELS_DIR) / 'tokenizer'
-    if not tokenizer_path.exists() or len(list(tokenizer_path.iterdir())) == 0:
-        print("Downloading tokenizer")
-        tokenizer_path = 'LanguageBind/Video-LLaVA-7B'
-
-    if load_8bit:
-        kwargs['load_in_8bit'] = True
-    elif load_4bit:
-        kwargs['load_in_4bit'] = True
-        kwargs['quantization_config'] = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.float16,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type='nf4'
-        )
-    else:
-        kwargs['torch_dtype'] = torch.float16
-
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, use_fast=False, cache_dir=kwargs["cache_dir"])
-    model = LlavaLlamaForCausalLM.from_pretrained(video_llava_path, low_cpu_mem_usage=True, **kwargs)
-    model.generation_config.do_sample = True
-
-    if save:
-        # save to on-disk paths
-        video_llava_path = Path(MODELS_DIR) / 'Video-LLaVA-7B'
-        tokenizer_path = Path(MODELS_DIR) / 'tokenizer'
-        tokenizer.save_pretrained(str(tokenizer_path))
-        model.save_pretrained(str(video_llava_path))
-    return model, tokenizer
